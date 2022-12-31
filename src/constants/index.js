683478c08eae17aa3473e1f4379afd5b8d72d78f
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-12-31 18:07:19 +0200

    config

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const appColors = {
+  GREEN: "#13d22d",
+  LIGHT_BLACK: "#17161e",
+  DARK_BLACK: "#121117",
+  GRAY: "#f3f3f3",
+  TEXT_WHITE: "#a1a1a3",
+  RED: "#fc3839",
+  TEXT_GRAY: "#676767",
+};
